51460295efeb6df80ba98f202f5fe30f334634af
Martijn Korbee <mkorbee@outlook.com>
2022-07-13 09:18:06 +0200

    moved PHP classes back seperate files

--- modal/database.php
+++ modal/database.php
@@ -1,18 +1,19 @@
 <?php 
 
-// class Database extends \mysqli {
-//     /*
-//     Database class that is responsible for and holds the connection.
-//     */
-//     public function __construct() {
-//         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-//         parent::__construct(
-//             $_ENV['DB_SERVER'],
-//             $_ENV['DB_USER'],
-//             $_ENV['DB_PASSWORD'],
-//             $_ENV['DB_NAME']
-//         );
-//     }
-// }    
+// CLASS DATABASE
+class Database extends \mysqli {
+    /*
+    Database class that is responsible for and holds the connection.
+    */
+    public function __construct() {
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        parent::__construct(
+            $_ENV['DB_SERVER'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASSWORD'],
+            $_ENV['DB_NAME']
+        );
+    }
+} 
 
 ?>
